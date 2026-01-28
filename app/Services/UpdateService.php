17e50b38ebfa6c1d351557b187be0ba79d60c3eb
TheoSfak <TheoSfak@users.noreply.github.com>
2026-01-28 11:16:25 +0200

    chore: bump version to 1.1.0

--- app/Services/UpdateService.php
+++ app/Services/UpdateService.php
@@ -11,7 +11,7 @@ class UpdateService
     /**
      * Τρέχουσα έκδοση της εφαρμογής
      */
-    const CURRENT_VERSION = '1.0.0';
+    const CURRENT_VERSION = '1.1.0';
     
     /**
      * GitHub repository
