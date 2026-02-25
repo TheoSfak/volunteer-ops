35b868d518c375afd0ba23aacbd1ffb780702bdf
TheoSfak <theodore.sfakianakis@gmail.com>
2026-02-25 09:08:13 +0200

    perf: add composite DB indexes + migration v21 + cleanup debug/test files v3.27.3

--- check_mission_template.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/bootstrap.php';
-
-$template = dbFetchOne('SELECT * FROM email_templates WHERE code = ?', ['mission_needs_volunteers']);
-echo json_encode($template, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
